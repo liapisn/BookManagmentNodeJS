5449b4453ad464db947bd14421f96f0b322766a2
liapisn <nikos@liapis.info>
2021-05-02 13:22:29 +0300

    clearing check boxes and results , create teble

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const sqlite3 = require('sqlite3').verbose();
-//create table books ( id integer primary key autoincrement , author text not null , title text not null , genre text not null , price integer not null );
+//create table books ( id integer primary key autoincrement , author text not null , title text not null , genre text not null , price float not null );
+let db = new sqlite3.Database('./db/books.db');
+db.run(`CREATE TABLE IF NOT EXISTS books ( id integer primary key autoincrement , author text not null , title text not null , genre text not null , price float not null )`)
+db.close();
 const express = require('express')
 const app = express()
 const cors = require('cors');
@@ -12,39 +15,37 @@ app.post('/books', (req, res) =>
 {
     const book = req.body;
     let db = new sqlite3.Database('./db/books.db');
-    db.run(`INSERT INTO BOOKS (author,title,genre,price) VALUES (?,?,?,?)`,[book.author, book.title, book.genre, book.price])
+    db.run(`INSERT INTO BOOKS (author,title,genre,price) VALUES (?,?,?,?)`, [book.author, book.title, book.genre, book.price])
     {
-        console.log("a book with title: "+book.title +" added");
+        console.log("a book with title: " + book.title + " added");
     }
-    res.send("a book with title: "+book.title +" added");
+    res.send("a book with title: " + book.title + " added");
     db.close();
 });
 
 app.get('/books/*', (req, res) => 
 {
     console.log(req.originalUrl);
-    let urlString=req.originalUrl;
+    let urlString = req.originalUrl;
     //const book = req.body;
     //console.log(book.title);
-    let key=urlString.slice(7)
+    let key = urlString.slice(7)
     console.log(key);
-    let keyword ='%'+key+'%';
+    let keyword = '%' + key + '%';
     let db = new sqlite3.Database('./db/books.db');
-    let sql = (`select * from books where title like ? or author like ? `);
-    db.all(sql, [keyword,keyword], (err, rows) =>
-    {
+    let sql = (`select * from books where title like ? `);
+    db.all(sql, [keyword], (err, rows) => {
         res.send(JSON.stringify(rows));
-        if (err) 
-        {
+        if (err) {
             throw err;
         }
-        console.log("Matching book titles for keyword '"+key+"' are:");
+        console.log("Here are all the matching book titles found for keyword '"+key+"'");
         rows.forEach((row) => 
         {
             console.log(row.title);
         });
     });
-    
+
     db.close();
 });
 
