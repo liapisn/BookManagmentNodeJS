5d2bf01c9ad0c8bdd60b7e4b5eb7808ed1a10440
liapisn <nikos@liapis.info>
2021-05-17 17:14:40 +0300

    frontend first touches

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('./db/books.db');
+// let userDB = new sqlite3.Database("./user1.db", 
+    // sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, 
+    // (err) => { 
+    //     // do your thing 
+    // });
+let db = new sqlite3.Database('./books.db');
 db.run(`CREATE TABLE IF NOT EXISTS books ( id integer primary key autoincrement , author text not null , title text not null , genre text not null , price float not null )`)
 db.close();
 const express = require('express')
@@ -12,7 +17,7 @@ app.use(parser.json());
 app.post('/books', (req, res) => 
 {
     const book = req.body;
-    let db = new sqlite3.Database('./db/books.db');
+    let db = new sqlite3.Database('./books.db');
     db.run(`INSERT INTO BOOKS (author,title,genre,price) VALUES (?,?,?,?)`, [book.author, book.title, book.genre, book.price])
     {
         console.log("a book with title: " + book.title + " added");
@@ -23,25 +28,29 @@ app.post('/books', (req, res) =>
 
 app.get('/books/*', (req, res) => 
 {
-    console.log(req.originalUrl);
     let urlString = req.originalUrl;
-    //const book = req.body;
-    //console.log(book.title);
     let key = urlString.slice(7)
-    console.log(key);
     let keyword = '%' + key + '%';
-    let db = new sqlite3.Database('./db/books.db');
+    let db = new sqlite3.Database('./books.db');
     let sql = (`select * from books where title like ? `);
     db.all(sql, [keyword], (err, rows) => {
-        res.send(JSON.stringify(rows));
-        if (err) {
-            throw err;
-        }
-        console.log("Here are all the matching book titles found for keyword '"+key+"'");
-        rows.forEach((row) => 
+        if(rows.length==0)
         {
-            console.log(row.title);
-        });
+            console.log("no books with key: "+key);
+            res.send(null);
+        }
+        else
+        {
+            res.send(JSON.stringify(rows));
+            if (err) {
+                throw err;
+            }
+            console.log("Here are all the matching book titles found for keyword '"+key+"'");
+            rows.forEach((row) => 
+            {
+                console.log(row.title);
+            });
+        }
     });
 
     db.close();
